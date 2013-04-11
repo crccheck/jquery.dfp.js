d6f8bbb5cf57092b86b13247e77eeae1bfbdca5a
crccheck <c@crccheck.com>
2013-04-11 06:34:47 +0000

    refactor: make blocked googletag object reusable

--- jquery.dfp.js
+++ jquery.dfp.js
@@ -24,6 +24,33 @@
     // Default DFP jQuery selector
     var dfpSelector = '.adunit';
 
+    // dummy implementation for googletag library
+    var dummyGoogletag = {
+        cmd: {
+            push: function (callback) {
+                callback.call(dfpScript);
+            }
+        },
+        ads: [],
+        pubads: function () { return this; },
+        enableSingleRequest: function () { return this; },
+        setTargeting: function () { return this; },
+        collapseEmptyDivs: function () { return this; },
+        enableServices: function () { return this; },
+        defineSlot: function (name, dimensions, id) {
+            window.googletag.ads.push(id);
+            window.googletag.ads[id] = {
+                renderEnded: function () {},
+                addService: function () { return this; }
+            };
+            return window.googletag.ads[id];
+        },
+        display: function (id) {
+            window.googletag.ads[id].renderEnded.call(dfpScript);
+            return this;
+        }
+    };
+
     /**
      * Init function sets required params and loads Google's DFP script
      * @param  String id       The DFP account ID
@@ -326,32 +353,7 @@
         setTimeout(function () {
 
             // overwrite the dfp object - replacing the command array with a function and defining missing functions
-            window.googletag = {
-                cmd: {
-                    push: function (callback) {
-                        callback.call(dfpScript);
-                    }
-                },
-                ads: [],
-                pubads: function () { return this; },
-                enableSingleRequest: function () { return this; },
-                setTargeting: function () { return this; },
-                collapseEmptyDivs: function () { return this; },
-                enableServices: function () { return this; },
-                defineSlot: function (name, dimensions, id) {
-                    window.googletag.ads.push(id);
-                    window.googletag.ads[id] = {
-                        renderEnded: function () {},
-                        addService: function () { return this; }
-                    };
-                    return window.googletag.ads[id];
-                },
-                display: function (id) {
-                    window.googletag.ads[id].renderEnded.call(dfpScript);
-                    return this;
-                }
-
-            };
+            window.googletag = dummyGoogletag;
 
             // Execute any stored commands
             $.each(commands, function (k, v) {
